f127b9b6b0fe5e7f5da64e313d5ab2e2a82b9621
Devon Ryan <dpryan79@gmail.com>
2022-04-19 19:46:23 +0200

    try using LD_LIBRARY_PATH to ensure perl finds the library

--- recipes/perl-devel-checklib/build.sh
+++ recipes/perl-devel-checklib/build.sh
@@ -2,6 +2,7 @@
 
 set -o errexit -o pipefail
 
+export LD_LIBRARY_PATH=$PREFIX/lib
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [[ -f Build.PL ]]; then
     perl Build.PL
@@ -19,9 +20,3 @@ else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
 fi
-
-# Add more build steps here, if they are necessary.
-
-# See
-# https://docs.conda.io/projects/conda-build
-# for a list of environment variables that are set during the build process.
